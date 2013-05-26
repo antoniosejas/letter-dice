8193227e6924e4cff953f5beba647893adf30b8f
Antonio Sejas <antonio@sejas.es>
2013-05-27 00:51:57 +0200

    Dado aleatorio en Javascript

--- index.php
+++ index.php
@@ -36,31 +36,50 @@
 				margin: 0;
 				padding: 0;
 			}
-			body {
+			html, body {
 				height: 100%;
 				width: 100%;
+				/*Impedimos que seleccionen texto*/
+				-webkit-touch-callout: none;
+				-webkit-user-select: none;
+				-khtml-user-select: none;
+				-moz-user-select: none;
+				-ms-user-select: none;
+				user-select: none;
 			}
 
 			 #letra {
-				width: 100%;
-				height: 50%;
-				position: relative;
-				top: 50%;
+			 	width: 100%;
+			 	position: relative;
+			 	height: 50%;
+			 	top: 25%;
 			}
 			h1 {
 				text-align: center;
 				font-size: 13em;
 				cursor: pointer;
+				text-transform:uppercase;
 			}
 			</style>		
 			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 			<meta name="apple-touch-fullscreen" content="YES">
 			<meta name="apple-mobile-web-app-capable" content="yes">
+			<script>
 
+			function getRandomInt(min, max) {
+			  return Math.floor(Math.random() * (max - min + 1)) + min;
+			}
+			var letras=new Array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+			
+			function cambiarLetra() {
+				document.getElementById("letrah1").innerHTML=letras[getRandomInt(0,letras.length-1)];
+			}
+
+			</script>
 		</head>
 		<body>
 			<div id="letra">
-				<h1 onclick="document.location.reload(true)"> <?php echo strtoupper($letras[rand(0,count($letras)-1)]); ?></h1>
+				<h1 id="letrah1" onclick="cambiarLetra();"> Click </h1>
 			</div>
 		</body>
 	</html>
